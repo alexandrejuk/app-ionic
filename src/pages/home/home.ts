5045eca8029daf21b2088c3238da175bb1c55571
alexandrejuk <ale_santos.soares@hotmail.com>
2018-07-10 15:05:35 -0300

    modify components in home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+@IonicPage({
+  name: 'HomePage',
+})
 export class HomePage {
 
-  public title="CONNECTA APP"
+  public title="Atendimentos"
 
 }
